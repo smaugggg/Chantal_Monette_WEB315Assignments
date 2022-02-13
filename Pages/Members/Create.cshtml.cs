594fc95fdcdcb424cb4de5143fc0cdd86c072d59
smaugggg <roborant@gmail.com>
2022-02-12 20:39:23 -0500

    Assignment 1 commit

---
+++ Pages/Members/Create.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DreamcatcherMembers.Models;
+
+namespace Dreamcatcher.Pages_Members
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DreamcatcherMembersContext _context;
+
+        public CreateModel(DreamcatcherMembersContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Member Member { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Member.Add(Member);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
