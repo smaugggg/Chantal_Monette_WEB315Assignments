fe4e9794c34739b9d35e9fc4ff551f4632a2b2af
smaugggg <roborant@gmail.com>
2022-03-27 20:06:30 -0400

    I have added the classes and made the grocery page

---
+++ ChantalMonetteGroceryApp/FoodItem.cs
@@ -0,0 +1,8 @@
+        public class FoodItem
+        {
+            public int quantity { get; set; }
+            public string department { get; set; }
+            public string typeOfFood { get; set; }
+            public string origin { get; set; }
+
+        }
---
+++ ChantalMonetteGroceryApp/GroceryIsle.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+
+public class GroceryIsle 
+{
+    public List<FoodItem> listofFoodItem { get; set; }
+    public int aisleNumber { get; set; }
+    public string aisleName { get; set; }
+}
